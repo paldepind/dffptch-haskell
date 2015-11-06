68845844a3503f551c80d1eb71a256411d10cb53
Simon Friis Vindum <simonfv@gmail.com>
2015-11-06 22:34:34 +0100

    Add nix files

---
+++ dffptch.nix
@@ -0,0 +1,14 @@
+{ mkDerivation, aeson, base, bytestring, scientific, stdenv, text
+, unordered-containers, vector
+}:
+mkDerivation {
+  pname = "dffptch";
+  version = "0.0.1.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson base bytestring scientific text unordered-containers vector
+  ];
+  homepage = "https://github.com/paldepind/dffptch-haskell";
+  description = "A small library for diffing and patching JSON objects with a compact diff format";
+  license = stdenv.lib.licenses.mit;
+}
