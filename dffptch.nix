4f16ffc0bfaa785ef94824f5012a78fdbcf8ef43
Simon Friis Vindum <simonfv@gmail.com>
2015-11-09 21:58:39 +0100

    Update nix file

--- dffptch.nix
+++ dffptch.nix
@@ -1,12 +1,13 @@
-{ mkDerivation, aeson, base, bytestring, scientific, stdenv, text
-, unordered-containers, vector
+{ mkDerivation, aeson, base, bytestring, hspec, scientific, stdenv
+, text, unordered-containers, vector
 }:
 mkDerivation {
   pname = "dffptch";
   version = "0.0.1.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base bytestring scientific text unordered-containers vector
+    aeson base bytestring hspec scientific text unordered-containers
+    vector
   ];
   homepage = "https://github.com/paldepind/dffptch-haskell";
   description = "A small library for diffing and patching JSON objects with a compact diff format";
